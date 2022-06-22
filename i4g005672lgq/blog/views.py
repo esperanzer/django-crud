cd959725b62536aaaf12c589fdc6061a05752bd3
esperanzer <esperanzatm95@gmail.com>
2022-06-22 23:32:26 +0200

    created post list view in blog app

--- i4g005672lgq/blog/views.py
+++ i4g005672lgq/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+# from django.views.generic.edit import CreateView
+from .models import Post
+from django.views.generic.list import ListView
 # Create your views here.
+
+
+
+class PostListView(ListView):
+    model=Post
