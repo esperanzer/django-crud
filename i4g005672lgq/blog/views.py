9330a8b32a211863ba0d0fd889eb94b8c20a1a44
esperanzer <esperanzatm95@gmail.com>
2022-06-23 00:16:34 +0200

    created detail and update views in blog app

--- i4g005672lgq/blog/views.py
+++ i4g005672lgq/blog/views.py
@@ -1,9 +1,13 @@
 from statistics import mode
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
 from .models import Post
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+
+
 # Create your views here.
 
 
@@ -15,5 +19,10 @@ class PostCreateView(CreateView):
     model = Post
     field ="__all__"
     success_url = reverse_lazy("blog:all")
+
+class DetailtView(DetailView):
+    model=Post
+
+class PostUpdateView(UpdateView):
 
 
