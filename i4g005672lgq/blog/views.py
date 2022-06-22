d8fb11020820f2cbf24dceacbabe6018f2e40d21
esperanzer <esperanzatm95@gmail.com>
2022-06-22 23:51:17 +0200

    created post create view view in blog app

--- i4g005672lgq/blog/views.py
+++ i4g005672lgq/blog/views.py
@@ -1,5 +1,7 @@
+from statistics import mode
 from django.shortcuts import render
-# from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 from .models import Post
 from django.views.generic.list import ListView
 # Create your views here.
@@ -8,3 +10,10 @@ from django.views.generic.list import ListView
 
 class PostListView(ListView):
     model=Post
+
+class PostCreateView(CreateView):
+    model = Post
+    field ="__all__"
+    success_url = reverse_lazy("blog:all")
+
+
